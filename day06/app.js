18df45ee4958038afcd980ce353fe911a41eabd4
Tubatiryaki <tubatiryaki25@gmail.com>
2023-01-05 21:03:45 +0300

    Day6 bitti -05.01.2023

--- day06/app.js
+++ day06/app.js
@@ -1,26 +1,23 @@
-
-const buttonMessage=document.querySelector(".btn-message");
-const buttonLike=document.querySelector(".btn-like");
+const buttonMessage = document.querySelector(".btn-message");
+const buttonLike = document.querySelector(".btn-like");
 const buttonFollow = document.querySelector(".btn-follow");
 
-const followerCount=document.querySelector(".count-follower");
-const postsCount=document.querySelector(".count-posts");
-const likesCount=document.querySelector(".count-likes");
+const followerCount = document.querySelector(".count-follower");
+const postsCount = document.querySelector(".count-posts");
+const likesCount = document.querySelector(".count-likes");
 
-let count= document.querySelector(".count-follower").innerText;
-let count2=document.querySelector(".count-posts").innerText;
-let count3=document.querySelector(".count-likes").innerText;
+let count = document.querySelector(".count-follower").innerText;
+let count2 = document.querySelector(".count-posts").innerText;
+let count3 = document.querySelector(".count-likes").innerText;
 
 // console.log(count);
 // let follower="146";
-buttonFollow.addEventListener("click",function(){
-    followerCount.innerHTML=count++;
-
-})
-buttonMessage.addEventListener("click",function(){
-    postsCount.innerHTML=count2++;
-})
-buttonLike.addEventListener("click",function(){
-    likesCount.innerHtml=count3++;
-    console.log(count3);
-})
+buttonFollow.addEventListener("click", function () {
+  followerCount.innerHTML = count++;
+});
+buttonMessage.addEventListener("click", function () {
+  postsCount.innerHTML = count2++;
+});
+buttonLike.addEventListener("click", function () {
+  likesCount.innerHTML = count3++;
+});
